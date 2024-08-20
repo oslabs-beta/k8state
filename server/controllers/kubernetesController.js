c460b8d05c5452c60dd0b98ecae602736c366bab
vincentcollis <vincent.collis@icloud.com>
2024-08-19 20:11:52 -0500

    commiting before merging with remove-files

---
+++ server/controllers/kubernetesController.js
@@ -0,0 +1,8 @@
+export const getPods = (req, res, next) => {
+};
+export const getPodDetails = (req, res, next) => {
+};
+export const getNodes = (req, res, next) => {
+};
+export const getServices = (req, res, next) => {
+};

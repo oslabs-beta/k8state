937dfbd6bdb263287034bc2185df98cc30b4e070
vincentcollis <vincent.collis@icloud.com>
2024-08-21 19:23:47 -0500

    commiting before merge
    Co-authored-by: Michael Chen <mochamochaccino@users.noreply.github.com>
    Co-authored-by: Jonathan Wu <Jon-Wu1@users.noreply.github.com>
    Co-authored-by: Alex <AlexG0718@users.noreply.github.com>

--- server/server.js
+++ server/server.js
@@ -1,16 +1,22 @@
 import express from 'express';
 import router from './routes/kubernetesRouter.js';
+
 const app = express();
 const PORT = 8080;
 // Middleware example
 app.use(express.json());
 // Kubernetes Router Handler
 app.use('/api', router);
-// Error handling middleware
-app.use((err, _req, res, _next) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
+// Kubernetes 404 Route Handler
+app.use('/', (_req, res) => {
+	res.status(404).send('Error page not found!');
 });
+// Express Global Error Handler
+app.use((err, _req, res, _next) => {
+	console.log(err);
+	res.status(500).send('Something broke!');
+});
+// Starts the app on the given port
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+	console.log(`Server is running on http://localhost:${PORT}`);
 });

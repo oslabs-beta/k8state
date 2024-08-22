3c77d487020516eb985a878d220c4878c83afd2e
Alex Greenberg <algreenberg89@gmail.com>
2024-08-21 20:23:49 -0400

    NavButton (app bar) complete, ClusterViewcontainer contains test chart from React Flows, and NavigationBarContainer contains NavButton (app bar) to containerize component

--- client/src/features/navigation-bar/containers/NavigationBarContainer.js
+++ client/src/features/navigation-bar/containers/NavigationBarContainer.js
@@ -1,1 +1,5 @@
-"use strict";
+import { jsx as _jsx } from "react/jsx-runtime";
+import NavButton from "../components/NavButton";
+export default function NavigationBarContainer() {
+    return (_jsx("div", { id: "navbar-container", className: "container", children: _jsx(NavButton, {}) }));
+}

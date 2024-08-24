0153ad9f575fd921dc0a966e1922067f3c50e99d
vincentcollis <vincent.collis@icloud.com>
2024-08-24 14:06:46 -0500

    deleted counter file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import "./App.css"
 import * as React from "react"
 import Button from "@mui/material/Button"
 
-import TestButton from "./testbutton"
+import TestButton from "./TestButton"
 
 const App = () => {
   return (

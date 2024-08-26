c419f8a0218fae2852ac2c8cbf249469c95cf221
vincentcollis <vincent.collis@icloud.com>
2024-08-26 10:20:48 -0500

    stable

--- client/src/features/cluster-view/containers/ClusterViewContainer.tsx
+++ client/src/features/cluster-view/containers/ClusterViewContainer.tsx
@@ -74,7 +74,7 @@ export default function ClusterViewContainer() {
 
   // **** dynamically create React Flow Edges ****
   const reactFlowEdges = (
-    reactFlowNodes: ReactFloNodeData[],
+    reactFlowNodes: ReactFlowNodeData[],
   ): ReactFlowEdgeFlow[] => {
     const outputArray = []
     let i = 1
@@ -89,6 +89,26 @@ export default function ClusterViewContainer() {
     }
     return outputArray
   }
+
+  // EXAMPLE REACT FLOW NODES AND EDGES
+
+  // const initialNodes = [
+  //   {
+  //     id: 'hidden-1',
+  //     type: 'input',
+  //     data: { label: 'Node 1' },
+  //     position: { x: 250, y: 5 },
+  //   },
+  //   { id: 'hidden-2', data: { label: 'Node 2' }, position: { x: 100, y: 100 } },
+  //   { id: 'hidden-3', data: { label: 'Node 3' }, position: { x: 400, y: 100 } },
+  //   { id: 'hidden-4', data: { label: 'Node 4' }, position: { x: 400, y: 200 } },
+  // ];
+
+  // const initialEdges = [
+  //   { id: 'hidden-e1-2', source: 'hidden-1', target: 'hidden-2' },
+  //   { id: 'hidden-e1-3', source: 'hidden-1', target: 'hidden-3' },
+  //   { id: 'hidden-e3-4', source: 'hidden-3', target: 'hidden-4' },
+  // ];
 
   const nodesToRender = reactFlowNodes()
   const edgesToRender = reactFlowEdges(nodesToRender)

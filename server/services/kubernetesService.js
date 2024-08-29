441667010f1e0fb61c5ebc13081bf6d11982ad62
Michael <156242839+mochamochaccino@users.noreply.github.com>
2024-08-28 19:12:43 -0500

    merge dev into branch

--- server/services/kubernetesService.js
+++ server/services/kubernetesService.js
@@ -89,8 +89,8 @@ const kubernetesService = {
             const test = await fetch('https://' + address + '/api/v1/nodes', {
                 method: 'GET',
                 headers: {
-                    authorization: 'Bearer ' + key,
-                },
+                    authorization: 'Bearer ' + key
+                }
             });
             //console.log(test);
             if (test.status !== 200) {

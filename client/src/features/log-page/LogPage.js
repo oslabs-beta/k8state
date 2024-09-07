9ea78883e66e659c3d26edcd06ee13a9ee76cbb9
vincentcollis <vincent.collis@icloud.com>
2024-09-07 11:08:34 -0500

    merging after pull request

--- client/src/features/log-page/LogPage.js
+++ client/src/features/log-page/LogPage.js
@@ -5,7 +5,7 @@ import Log from "./Log";
 export default function LogPage() {
     const [dirInfo, setdirInfo] = useState([]);
     const [log, setLog] = useState([]);
-    const [deleted, setDeleted] = useState('');
+    const [deleted, setDeleted] = useState("");
     useEffect(() => {
         fetch("http://localhost:8080/api/getLogs")
             .then(process => process.json())
@@ -19,7 +19,7 @@ export default function LogPage() {
     }, [log, deleted]);
     const createLogHandler = () => {
         fetch("http://localhost:8080/api/createLogs", {
-            method: 'POST'
+            method: "POST",
         })
             .then(process => process.json())
             .then(data => {
@@ -41,15 +41,23 @@ export default function LogPage() {
     //     // </Grid>);
     // )};
     for (let i = dirInfo.length; i > 0; i--) {
-        store.push(
-        // <Grid item xs={12} sm={6} md={4} key={i + 3013031}>
-        // <Box sx={{ border: 1, borderColor: "black"}}>
-        _jsx(Box, { children: _jsx(Log, { setDeleted: setDeleted, logName: dirInfo[i] }) }, i * 123)
-        // </Grid>
-        );
+        if (dirInfo[i] !== (null || undefined)) {
+            store.push(
+            // <Grid item xs={12} sm={6} md={4} key={i + 3013031}>
+            // <Box sx={{ border: 1, borderColor: "black"}}>
+            _jsx(Box, { children: _jsx(Log, { setDeleted: setDeleted, logName: dirInfo[i] }) }, i * 123));
+        }
     }
-    ;
     console.log(store);
-    return (_jsxs("div", { children: [_jsx("h1", { style: { marginLeft: '32px', marginBottom: '16px' }, children: "Logs" }), _jsx(Button, { style: { marginLeft: '32px', marginBottom: '16px' }, variant: "contained", color: "primary", type: "button", onClick: createLogHandler, children: "Create a Log" }), store] }));
+    return (_jsxs("div", { style: { position: "absolute", left: "250px", top: "100px" }, children: [_jsx("h1", { style: {
+                    textAlign: "center",
+                    marginLeft: "32px",
+                    marginBottom: "16px",
+                    minWidth: "700px",
+                }, children: "Logs" }), _jsx(Button, { style: {
+                    display: "flex",
+                    left: "290px",
+                    marginLeft: "32px",
+                    marginBottom: "16px",
+                }, variant: "contained", color: "primary", type: "button", onClick: createLogHandler, children: "Create a Log" }), store] }));
 }
-;

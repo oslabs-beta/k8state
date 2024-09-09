7fc0fa05822c3b7ca42633eb38d5b8563a5d14c7
vincentcollis <vincent.collis@icloud.com>
2024-09-09 12:17:14 -0500

    stable after merege

--- client/src/features/settings/settings.js
+++ client/src/features/settings/settings.js
@@ -1,24 +1,20 @@
 import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import Alert from "@mui/material/Alert";
 import { useState } from "react";
 const Settings = () => {
-    // state for mode option functionality
-    // const [selectedOption, setSelectedOption] = useState<string>(
-    //   "Kubernetes API (default)",
-    // )
-    // setting selector function for mode option functionality
-    // const handleSettingSelect = (event: React.ChangeEvent<HTMLInputElement>) => {
-    //   setSelectedOption(event.target.value)
-    // }
-    const [envOption, setEnvOption] = useState(false);
+    const [inputError, setInputError] = useState(false);
     const [envAddress, setEnvAddress] = useState(null);
     const [envKey, setEnvKey] = useState(null);
-    const [envToolTip, setEnvToolTip] = useState(null);
-    const [envToolTipMessage, setEnvToolTipMessage] = useState(null);
-    const handleEditClick = () => {
-        setEnvOption(envOption === false ? true : false);
-    };
+    const [envAlertMessage, setEnvAlertMessage] = useState(null);
+    const [envAlert, setEnvAlert] = useState(false);
     const handleEnvSubmit = (event) => {
         event.preventDefault();
+        if (inputError === true || envAlertMessage === "Success!")
+            return;
         fetch("http://localhost:8080/api/checkAPI", {
             method: "POST",
             headers: {
@@ -32,25 +28,36 @@ const Settings = () => {
             .then(response => response.json())
             .then(data => {
             if (data.message === "ok") {
-                setEnvToolTipMessage("Success!");
-                setEnvToolTip(true);
-                setTimeout(() => setEnvToolTip(false), 5000);
+                setEnvAlertMessage("Success!");
+                setEnvAlert(true);
+                setTimeout(() => setEnvAlert(false), 5000);
+                setTimeout(() => setEnvAlertMessage(null), 5000);
             }
             else {
-                setEnvToolTipMessage("Invalid Address or Key");
-                setEnvToolTip(true);
-                setTimeout(() => setEnvToolTip(false), 5000);
+                setEnvAlertMessage("Invalid Address or Key");
+                setEnvAlert(true);
+                setInputError(true);
+                setTimeout(() => setEnvAlert(false), 5000);
+                setTimeout(() => setInputError(false), 5000);
+                setTimeout(() => setEnvAlertMessage(null), 5000);
             }
         });
-        setEnvOption(false);
     };
-    return (_jsx("div", { className: "container", id: "settings-container", style: {
-            position: "sticky",
-            marginTop: "100px",
-            marginLeft: "300px",
-            textAlign: "center",
-            backgroundColor: "#ac96cf",
-            paddingBottom: "20px",
-        }, children: _jsx("div", { className: "container", id: "env-settings-container", style: { margin: "15px", padding: "5px" }, children: _jsxs("section", { children: [_jsx("h2", { style: { textDecoration: "underline" }, children: "Settings for API access" }), _jsxs("form", { className: "env-settings", id: "env-settings-form", children: [_jsx("br", {}), _jsx("label", { style: { fontWeight: "bold" }, children: "Set new Address and Key" }), _jsx("br", {}), _jsx("button", { type: "button", style: { borderRadius: "10px", marginTop: "10px" }, onClick: handleEditClick, children: "Edit" }), _jsx("br", {}), _jsxs("div", { className: "container", id: "env-settings-input", style: { marginTop: "20px" }, children: [envOption && (_jsx("input", { type: "text", placeholder: "Cluster Address", onChange: (e) => setEnvAddress(e.target.value) })), _jsx("br", {}), envOption && (_jsx("input", { type: "text", placeholder: "Cluster Key", onChange: (e) => setEnvKey(e.target.value) })), _jsx("br", {}), envOption && (_jsx("button", { type: "submit", onClick: e => handleEnvSubmit(e), children: "Submit" })), _jsx("br", {}), envToolTip && (_jsx("div", { className: "settings-tooltip", id: "settings-env-tooltip", style: { marginTop: "10px" }, children: envToolTipMessage }))] })] })] }) }) }));
+    return (_jsxs(Box, { component: "form", sx: { "& > :not(style)": { m: 1, width: "25ch" } }, noValidate: true, autoComplete: "off", children: [_jsx(TextField, { label: "Address", color: envAlertMessage === "Success!" ? "success" : "primary", error: inputError, onChange: (e) => setEnvAddress(e.target.value), focused: true, style: { position: "fixed", top: "200px", left: "500px" } }), _jsx(TextField, { label: "Key", color: envAlertMessage === "Success!" ? "success" : "primary", error: inputError, onChange: (e) => setEnvKey(e.target.value), focused: true, style: { position: "fixed", top: "280px", left: "500px" } }), _jsx("div", { style: {
+                    display: "flex",
+                    position: "absolute",
+                    top: "350px",
+                    left: "570px",
+                }, children: _jsx(Stack, { direction: "row", spacing: 2, children: _jsx(Button, { variant: inputError === true ? "outlined" : "contained", color: inputError === true
+                            ? "error"
+                            : envAlertMessage === "Success!"
+                                ? "success"
+                                : "primary", onClick: handleEnvSubmit, style: { marginTop: "16px" }, children: "Submit" }) }) }), _jsx("div", { style: {
+                    display: "flex",
+                    position: "absolute",
+                    left: "500px",
+                    top: "425px",
+                    marginTop: "16px",
+                }, children: _jsx(Stack, { sx: { width: "100%" }, spacing: 2, children: envAlert && (_jsx(Alert, { severity: inputError === true ? "error" : "success", children: envAlertMessage })) }) })] }));
 };
 export default Settings;

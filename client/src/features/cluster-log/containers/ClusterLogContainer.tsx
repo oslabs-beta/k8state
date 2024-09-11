db17560433efe5404ff944d7f1e685153425bd4f
Alex Greenberg <algreenberg89@gmail.com>
2024-09-11 15:56:45 -0400

    Minor adjustment

--- client/src/features/cluster-log/containers/ClusterLogContainer.tsx
+++ client/src/features/cluster-log/containers/ClusterLogContainer.tsx
@@ -117,7 +117,7 @@ export default function LogPage() {
     <div
       style={{
         position: "relative",
-        left: "235px",
+        left: "230px",
         top: "90px",
         width: "100vw",
         height: "100vh",
